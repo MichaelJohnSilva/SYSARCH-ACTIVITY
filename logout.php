a609818cc2ec033362c16b4c11ad5243bf35ca8f
Michael John Silva <michaeljohnsilva5@gmail.com>
2026-04-17 13:49:52 +0900

    Keep only SYSARCH-ACTIVITY folder

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include "config.php";
-
-session_destroy();
-header("Location: login.php");
-exit();
-?>
--- update_reservations.sql
+++
@@ -1,2 +0,0 @@
--- Add computer_number column to reservations table
-ALTER TABLE reservations ADD COLUMN computer_number VARCHAR(10) AFTER lab;
